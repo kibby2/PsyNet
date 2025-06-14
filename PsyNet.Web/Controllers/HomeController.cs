6c5ec612105f1afa3c49628dc35e1921e55d9224
kibby2 <sar1439@my.londonmet.ac.uk>
2025-06-14 18:14:34 +0545

    minor fix

--- PsyNet.Web/Controllers/HomeController.cs
+++ PsyNet.Web/Controllers/HomeController.cs
@@ -6,7 +6,8 @@ using PsyNet.Web.Models.ViewModels;
 using PsyNet.Web.Repositories;
 
 namespace PsyNet.Web.Controllers
-{    public class HomeController : Controller
+{
+    public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogPostRepository blogPostRepository;
@@ -41,7 +42,8 @@ namespace PsyNet.Web.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]        public IActionResult Error()
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
