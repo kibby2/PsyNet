5fb3e6124f409ccf92bcddb06ea8f33c0614f3cd
kibby2 <sar1439@my.londonmet.ac.uk>
2025-06-14 19:48:31 +0545

    comments validation

--- PsyNet.Web/Controllers/BlogsController.cs
+++ PsyNet.Web/Controllers/BlogsController.cs
@@ -24,10 +24,12 @@ namespace PsyNet.Web.Controllers
             this.userManager = userManager;
             this.blogPostCommentRepository = blogPostCommentRepository;
         }
-
         [HttpGet]
         public async Task<IActionResult> Index(string urlHandle)
         {
+            // Clear any validation state from redirects
+            ModelState.Clear();
+
             var liked = false;
             var blogPost = await blogPostRepository.GetByUrlHandleAsync(urlHandle);
             var blogDetailsViewModel = new BlogDetailsViewModel();
@@ -50,9 +52,7 @@ namespace PsyNet.Web.Controllers
                     }
                 }
                 //Get commenys for blog post
-                var blogCommentsDomainModel = await blogPostCommentRepository.GetCommentsByBlogIdAsync(blogPost.Id);
-
-                var blogCommentsForView = new List<BlogComment>(); foreach (var blogComment in blogCommentsDomainModel)
+                var blogCommentsDomainModel = await blogPostCommentRepository.GetCommentsByBlogIdAsync(blogPost.Id); var blogCommentsForView = new List<BlogComment>(); foreach (var blogComment in blogCommentsDomainModel)
                 {
                     var commentUser = await userManager.FindByIdAsync(blogComment.UserId.ToString());
                     blogCommentsForView.Add(new BlogComment
@@ -60,8 +60,8 @@ namespace PsyNet.Web.Controllers
                         Id = blogComment.Id,
                         Description = blogComment.Description,
                         DateAdded = blogComment.DateAdded,
-                        Username = commentUser.UserName,
-                        ProfilePictureUrl = commentUser.ProfilePictureUrl
+                        Username = commentUser?.UserName ?? "Unknown User",
+                        ProfilePictureUrl = commentUser?.ProfilePictureUrl
                     });
                 }
                 blogDetailsViewModel = new BlogDetailsViewModel
@@ -79,32 +79,89 @@ namespace PsyNet.Web.Controllers
                     Tags = blogPost.Tags,
                     TotalLikes = totalLikes,
                     Liked = liked,
-                    Comments = blogCommentsForView
+                    Comments = blogCommentsForView,
+                    CommentDescription = string.Empty
                 };
             }
 
             return View(blogDetailsViewModel);
         }
-
         [HttpPost]
         public async Task<IActionResult> Index(BlogDetailsViewModel blogDetailsViewModel)
         {
             if (signInManager.IsSignedIn(User))
             {
-                var domainModel = new BlogPostComment
+                if (!ModelState.IsValid)
                 {
-                    BlogPostId = blogDetailsViewModel.Id,
-                    Description = blogDetailsViewModel.CommentDescription,
-                    UserId = Guid.Parse(userManager.GetUserId(User)),
-                    DateAdded = DateTime.Now
-                };
-                await blogPostCommentRepository.AddAsync(domainModel);
-                return RedirectToAction("Index", "Blogs",
-                    new { urlHandle = blogDetailsViewModel.UrlHandle });
+                    // If validation fails, reload the blog post data and return the view with errors
+                    var blogPost = await blogPostRepository.GetByUrlHandleAsync(blogDetailsViewModel.UrlHandle);
+                    if (blogPost != null)
+                    {
+                        var totalLikes = await blogPostLikeRepository.GetTotalLikes(blogDetailsViewModel.Id);
+                        var liked = false;                        // Get like status for this user
+                        var likesForBlog = await blogPostLikeRepository.GetLikesForBlog(blogPost.Id);
+                        var currentUserId = userManager.GetUserId(User);
+                        if (currentUserId != null)
+                        {
+                            var likeFromUser = likesForBlog.FirstOrDefault(x => x.UserId == Guid.Parse(currentUserId));
+                            liked = likeFromUser != null;
+                        }
+
+                        var blogCommentsForView = new List<BlogComment>();
+                        var blogComments = await blogPostCommentRepository.GetCommentsByBlogIdAsync(blogPost.Id);
+                        foreach (var blogComment in blogComments)
+                        {
+                            var user = await userManager.FindByIdAsync(blogComment.UserId.ToString());
+                            blogCommentsForView.Add(new BlogComment
+                            {
+                                Description = blogComment.Description,
+                                DateAdded = blogComment.DateAdded,
+                                Username = user?.UserName ?? "Unknown User",
+                                ProfilePictureUrl = user?.ProfilePictureUrl,
+                                Id = blogComment.Id
+                            });
+                        }
+
+                        var blogDetailsViewModelWithData = new BlogDetailsViewModel
+                        {
+                            Id = blogPost.Id,
+                            Content = blogPost.Content,
+                            PageTitle = blogPost.PageTitle,
+                            Author = blogPost.Author,
+                            FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                            Heading = blogPost.Heading,
+                            PublishedDate = blogPost.PublishedDate,
+                            ShortDescription = blogPost.ShortDescription,
+                            UrlHandle = blogPost.UrlHandle,
+                            Visible = blogPost.Visible,
+                            Tags = blogPost.Tags,
+                            TotalLikes = totalLikes,
+                            Liked = liked,
+                            Comments = blogCommentsForView,
+                            CommentDescription = blogDetailsViewModel.CommentDescription
+                        };
+
+                        return View(blogDetailsViewModelWithData);
+                    }
+                }
+
+                var userId = userManager.GetUserId(User);
+                if (userId != null)
+                {
+                    var domainModel = new BlogPostComment
+                    {
+                        BlogPostId = blogDetailsViewModel.Id,
+                        Description = blogDetailsViewModel.CommentDescription,
+                        UserId = Guid.Parse(userId),
+                        DateAdded = DateTime.Now
+                    };
+                    await blogPostCommentRepository.AddAsync(domainModel);
+                    return RedirectToAction("Index", "Blogs",
+                        new { urlHandle = blogDetailsViewModel.UrlHandle });
+                }
             }
 
             return View();
-
         }
     }
 }
--- PsyNet.Web/Models/ViewModels/BlogDetailsViewModel.cs
+++ PsyNet.Web/Models/ViewModels/BlogDetailsViewModel.cs
@@ -1,27 +1,28 @@
 ﻿using PsyNet.Web.Models.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace PsyNet.Web.Models.ViewModels
 {
     public class BlogDetailsViewModel
     {
         public Guid Id { get; set; }
-        public string Heading { get; set; }
-        public string PageTitle { get; set; }
-        public string Content { get; set; }
-        public string ShortDescription { get; set; }
-        public string FeaturedImageUrl { get; set; }
-        public string UrlHandle { get; set; }
+        public string Heading { get; set; } = string.Empty;
+        public string PageTitle { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string ShortDescription { get; set; } = string.Empty;
+        public string FeaturedImageUrl { get; set; } = string.Empty;
+        public string UrlHandle { get; set; } = string.Empty;
         public DateTime PublishedDate { get; set; }
-        public string Author { get; set; }
+        public string Author { get; set; } = string.Empty;
         public bool Visible { get; set; }
-        public ICollection<Tag> Tags { get; set; }
+        public ICollection<Tag> Tags { get; set; } = new List<Tag>();
 
         public int TotalLikes { get; set; }
-
         public bool Liked { get; set; }
+        [Required(ErrorMessage = "Comment description is required")]
+        [StringLength(500, MinimumLength = 1, ErrorMessage = "Comment must be between 1 and 500 characters")]
+        public string CommentDescription { get; set; } = string.Empty;
 
-        public string CommentDescription { get; set; }
-
-        public IEnumerable<BlogComment> Comments { get; set; }
+        public IEnumerable<BlogComment> Comments { get; set; } = new List<BlogComment>();
     }
 }
